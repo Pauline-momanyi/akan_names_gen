592c4356aa4bf84d96b6d4e0260f2a61ceded9b4
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-03-06 11:24:20 +0300

    Link the js file to index.html. Declare variables for day, month, year and gender

---
+++ script.js
@@ -0,0 +1,10 @@
+var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var FemaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+function getName(){
+let day=parseInt(document.getElementById('day').value);
+let month=parseInt(document.getElementById('month').value);
+let fullyear=parseInt(document.getElementById('year').value);
+let gender=document.getElementById('gender').value;
+
+
+}
